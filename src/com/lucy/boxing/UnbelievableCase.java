f3ed78d790a2b8251c18fbdeb279394f62e69a59
haebin.lee <luckylucy0717@gmail.com>
2021-02-10 21:16:58 +0900

    item61 박싱타입의 오류 케이스

---
+++ src/com/lucy/boxing/UnbelievableCase.java
@@ -0,0 +1,11 @@
+package com.lucy.boxing;
+
+public class UnbelievableCase {
+    static Integer i;
+
+    public static void main(String[] args) {
+        if (i == 42) {
+            System.out.println("믿을 수 없군!");
+        }
+    }
+}
